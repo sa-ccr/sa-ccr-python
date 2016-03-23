d9011543f1f11b3ce06fb3e4d57aabc546a75301
Vassilios Rendoumis <vassilios.rendoumis@gmail.com>
2016-03-23 16:41:43 +0200

    Create Vol.py

---
+++ Vol.py
@@ -0,0 +1,4 @@
+######################################################################
+# Create the base Vol class
+#
+# This is used to represent the swap product and it will contained to all the swap-like classes

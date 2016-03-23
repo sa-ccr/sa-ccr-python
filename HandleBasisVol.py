dcf540fe92a18faf7b11156bc99518f6c2e9d6e0
Vassilios Rendoumis <vassilios.rendoumis@gmail.com>
2016-03-23 16:40:56 +0200

    Create HandleBasisVol.py

---
+++ HandleBasisVol.py
@@ -0,0 +1,2 @@
+# Calculates the addon amount after splitting the trades into 'basis swap', 'volatility' and 'normal' transactions.
+# The corresponding penalty factors are applied to the supervisory factors for each trade group.
